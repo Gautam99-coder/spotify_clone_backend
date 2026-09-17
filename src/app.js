15d27ad989e574e86c9c29cb7693110d19631786
Gautam99-coder <186465364+Gautam99-coder@users.noreply.github.com>
2026-09-17 23:50:59 +0530

    initialize server and app files with basic setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const  express=require("express")
+const cookie_parser=require("cookie-parser")
+const authRouter=require("./routes/auth.routes")
+const musicRouter=require("./routes/music.routes")
+
+const app=express()
+
+
+app.use(express.json())
+app.use(cookie_parser())
+
+
+app.use("/api/auth/",authRouter);
+app.use("/api/auth",musicRouter);
+
+
+module.exports=app;
