5cc9ef3bdb653e8bb2e2cd2ec81687a88af7874c
Gautam99-coder <186465364+Gautam99-coder@users.noreply.github.com>
2026-09-18 23:12:05 +0530

    feat: add authentication middleware to music routes

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,12 +1,19 @@
 const express=require("express")
 const musicController=require("../controllers/music.controller")
+const authMiddleWare=require("../middlewares/auth.middleware")
 const multer=require("multer")
+
 
 const upload=multer({
     storage:multer.memoryStorage(),
 })
 const router=express.Router();
 
-router.post("/create-music",upload.single("music"), musicController.createMusic);
+router.post("/create-music",authMiddleWare.authArtist,upload.single("music"), musicController.createMusic);
+
+router.post("/create-album",authMiddleWare.authArtist,musicController.createAlbum);
+router.get("/musics",authMiddleWare.authUser,musicController.getAllMusics)
+router.get("/album",musicController.getAlbum);
+router.get("/album/:albumId",musicController.getAlbumById);
 
 module.exports=router;
